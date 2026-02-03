e04de3d6802e2a8ac340a61bd0e7f1f4d50c974d
Rahma Al-Wadhahi <61970868+Rahma-AlWadhahi@users.noreply.github.com>
2026-02-03 11:52:35 +0400

    add color coded stages for the portal user

--- addons-custom/itsm_support/models/stage.py
+++ addons-custom/itsm_support/models/stage.py
@@ -6,6 +6,15 @@ class ITSMStage(models.Model):
     _description = 'Ticket Stage'
     _order = 'sequence'
 
+    color = fields.Selection([
+        ('secondary','Gray'),
+        ('primary','Blue'),
+        ('warning','Orange'),
+        ('info','Cyan'),
+        ('success','Green'),
+        ('danger','Red'),
+    ], default='secondary')
+
     name = fields.Char(required=True)
     sequence = fields.Integer(default=10)
     fold = fields.Boolean(string='Folded in Kanban')
